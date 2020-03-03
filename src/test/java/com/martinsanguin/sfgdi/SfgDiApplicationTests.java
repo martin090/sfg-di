8715f1654ee7551cf5be8550ce4e286f25324933
Martin Sanguin <sanguinmartin@gmail.com>
2020-03-03 01:03:01 -0300

    Initial commit.

---
+++ src/test/java/com/martinsanguin/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinsanguin.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
